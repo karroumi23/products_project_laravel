d7a9336f0dbd685bb5fabd6baba0100916e90abc
karroumi23 <anasskarroumi2016@gmail.com>
2026-06-29 10:36:08 +0200

    make logo in backoffice change with mode light or dark & change primary color backoffice

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,18 +18,28 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
+
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->default()
-            ->id('admin')
-            ->path('admin')
-            ->login()
-            ->colors([
-                'primary' => Color::Amber,
-            ])
+                ->default()
+                ->id('admin')
+                ->path('admin')
+                ->login()
+
+                // ->brandLogo(asset('images/logo-light.png'))
+                ->brandLogo(asset('images/logo-dark.png'))
+                ->darkModeBrandLogo(asset('images/logo-light.png'))
+                ->brandLogoHeight('50px')
+                ->brandName('')
+
+                ->colors([
+                    'primary' => Color::Red,
+                  ])
+
+
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
@@ -38,7 +48,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
